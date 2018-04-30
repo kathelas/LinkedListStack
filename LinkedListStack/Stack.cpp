a7806817a89ebfa071f3283630720c20fe1a2f5d
kathelas <kathelaswow@gmail.com>
2018-04-30 17:33:49 +0200

    seems all good, all tests done (incl test 9)

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,57 @@
 #include "Stack.h"
+
+void Stack::Push( int val )
+{
+	if( Size() == 0 )
+	{
+		top = new Element{ val };
+	}
+	else
+	{
+		Element* oldtop = top; //old top element
+		top = new Element{ val, oldtop };
+		oldtop->next = top;
+	}
+}
+
+int Stack::Pop()
+{
+	if( Size() == 0 )
+		return -1;
+	else
+	{
+		Element* newtop = top->prev;
+		int rvalue = top->content;
+		delete top;
+		top = newtop;
+		if( top )
+			top->next = nullptr;
+		return rvalue;
+	}
+}
+
+int Stack::Size() const
+{
+	int count = 0;
+	if( !top )
+		return count;
+	else
+	{
+		Element* temp = top;
+		count++;
+		while( temp->prev )
+		{
+			temp = temp->prev;
+			count++;
+		}
+		return count;
+	}
+}
+
+bool Stack::Empty() const
+{
+	if( Size() == 0 )
+		return true;
+	else
+		return false;
+}
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -3,8 +3,76 @@
 class Stack
 {
 public:
+	Stack() = default;
+	~Stack()
+	{
+		while( Pop() != -1 )
+		{
+		}
+
+	}
+	Stack( const Stack& other )
+	{
+		Element* temp = other.top;
+		while( temp->prev != nullptr )
+		{
+			temp = temp->prev;
+		}
+		if( temp )
+		{
+			Push( temp->content );
+			while( temp->next )
+			{
+				temp = temp->next;
+				Push( temp->content );
+			}
+		}
+	}
+	Stack& operator=( const Stack& other )
+	{
+		while( Pop() != -1 )
+		{
+		}
+
+		Element* temp = other.top;
+		while( temp->prev )
+		{
+			temp = temp->prev;
+		}
+		if( temp != nullptr )
+		{
+			Push( temp->content );
+			while( temp->next )
+			{
+				temp = temp->next;
+				Push( temp->content );
+			}
+		}
+
+		return *this;
+	}
+
 	void Push( int val );
 	int Pop();
 	int Size() const;
 	bool Empty() const;
+private:
+	struct Element
+	{
+		Element() = default;
+		Element( int val )
+		{
+			content = val;
+		}
+		Element( int val, Element* ptr )
+		{
+			content = val;
+			prev = ptr;
+		}
+
+		int content;
+		Element* prev = nullptr;
+		Element* next = nullptr;
+	};
+	Element* top = nullptr;
 };
